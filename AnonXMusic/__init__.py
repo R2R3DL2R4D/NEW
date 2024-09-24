6c2a9253b0aef5779f44690202f3709525926240
R2R3DL2R4D <rahulkumartarapur95@gmail.com>
2024-09-24 10:07:00 +0530

    Add files via upload

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -1,1 +1,25 @@
+from AnonXMusic.core.bot import Anony
+from AnonXMusic.core.dir import dirr
+from AnonXMusic.core.git import git
+from AnonXMusic.core.userbot import Userbot
+from AnonXMusic.misc import dbb, heroku, sudo
 
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+# sudo()
+
+app = Anony()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
