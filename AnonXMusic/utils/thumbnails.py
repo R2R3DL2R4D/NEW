9b9d90bfe77233f8a6a4e03d631a30812637c9bc
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-10-15 16:33:42 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -11,7 +11,7 @@ from PIL import Image, ImageChops, ImageOps, ImageDraw, ImageEnhance, ImageFilte
 from youtubesearchpython.__future__ import VideosSearch
 
 from AnonXMusic import app
-from AnonXMusic.resource import thumbs, colors
+from resource import thumbs, colors
 from config import YOUTUBE_IMG_URL
 
 
