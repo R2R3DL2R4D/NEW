6c7ec19706963b5884e2be5d28546d6c73e1fa12
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-10-19 02:16:27 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -92,7 +92,7 @@ async def get_thumb(videoid,user_id):
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.5)
 
-image3 = changeImageSize(1280, 720, bg)
+        image3 = changeImageSize(1280, 720, bg)
         image5 = image3.convert("RGBA")
         Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
         y=changeImageSize(365,365,circle(youtube)) 
